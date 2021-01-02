464fb2f395816da423876655365c20e72d8042fa
Nicelx <Relush321@gmail.com>
2021-01-02 23:50:03 +0300

    jQuery things

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,10 +1,55 @@
-$(document).ready(() => {
-	$('a').click((event) => {
-		event.preventDefault();
-		$(this).hide('slow')
+// $(document).ready(() => {
+// 	$('a').click((event) => {
+// 		event.preventDefault();
+// 		$(this).hide('slow')
+// 	})
+// 	$('div').click(function() {
+// 		$(this).hide('slow')
+// 	})
+// 	$('a').addClass('test');
+// })
+
+
+
+
+// $(() => {
+// 	$('div').html('ready')
+// })
+
+// $( document ).ready(function() {
+// 	console.log( "document loaded" );
+// });
+
+// $( window ).on( "load", function() {
+// 	console.log( "window loaded" );
+// });
+
+
+
+// let $j = jQuery.noConflict();
+// console.log($j)
+
+// $j('div').click(function() {
+// 	this.fadeToggle('slow', "linear")
+// })
+
+
+// $(() => {
+// 	$('a').attr({
+// 		href: "https://api.jquery.com/fadeToggle/",
+// 		style: "text-decoration: none" 
+// 	})
+// })
+
+
+
+
+$(() => {
+	$('a, div').click(() => {
+		console.log('a or div clicked')
 	})
-	$('div').click(function() {
-		$(this).hide('slow')
-	})
-	$('a').addClass('test');
 })
+
+
+
+
