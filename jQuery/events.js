9a7ac3662354dc1b0107e99b9eb835c2b138b4b6
Nicelx <37116427+Nicelx@users.noreply.github.com>
2021-01-14 16:49:09 +0300

    events done

--- jQuery/events.js
+++ jQuery/events.js
@@ -58,18 +58,90 @@ const foo = () => console.log("foo function fired");
 // $('p.content').click(event => console.log(event))
 // $('p.content').click(event => console.log(event.originalEvent))
 
-
 // many events and handlers
-$( "p.content" ).on({
-    mouseenter: function() {
-        $(this).addClass('magic');
-    },
-    mouseleave: function() {
-		$(this).addClass('secondMagic')
-	},
-    click: function(event) {
-		console.dir( event );
-		$(this).removeClass(['magic', 'secondMagic'])
-		$(this).off()
-    }
-});
+// $( "p.content" ).on({
+//     mouseenter: function() {
+//         $(this).addClass('magic');
+//     },
+//     mouseleave: function() {
+// 		$(this).addClass('secondMagic')
+// 	},
+//     click: function(event) {
+// 		console.dir( event );
+// 		$(this).removeClass(['magic', 'secondMagic'])
+// 		$(this).off()
+//     }
+// });
+
+// Binding events to elements that don't exist yet
+// $( "ul" ).on( "click", "li", function() {
+//     console.log( "Something in a <ul> was clicked, and we detected that it was an <li> element." );
+// });
+
+// trigger
+// $( "p.content" ).click(() => alert('clicked'));
+// $( "p.content" ).trigger( "click" ); // Don't use .trigger() simply to execute specific functions
+// $( "p.content" ).triggerHandler( "click" ); // have some diff from .trigger()
+
+// Triggering a native browser event using the simulate plugin NEED PLUGIN!
+// $( "p.content" ).simulate( "click" );
+
+// custom events
+// example without customs events
+// $( ".switch, .clapper" ).click(function() {
+//     var light = $( this ).closest( ".room" ).find( ".lightbulb" );
+//     if ( light.is( ".on" ) ) {
+//         light.removeClass( "on" ).addClass( "off" );
+//     } else {
+//         light.removeClass( "off" ).addClass( "on" );
+//     }
+// });
+
+// some example custom events
+// $(".lightbulb").on("light:toggle", function (event) {
+// 	var light = $(this);
+// 	if (light.is(".on")) {
+// 		light.removeClass("on").addClass("off");
+// 	} else {
+// 		light.removeClass("off").addClass("on");
+// 	}
+// });
+
+// $(".switch, .clapper").click(function () {
+// 	var room = $(this).closest(".room");
+// 	room.find(".lightbulb").trigger("light:toggle");
+// });
+
+// with master switch
+// $(".lightbulb")
+// 	.on("light:toggle", function (event) {
+// 		var light = $(this);
+// 		if (light.is(".on")) {
+// 			light.trigger("light:off");
+// 		} else {
+// 			light.trigger("light:on");
+// 		}
+// 	})
+// 	.on("light:on", function (event) {
+// 		$(this).removeClass("off").addClass("on");
+// 	})
+// 	.on("light:off", function (event) {
+// 		$(this).removeClass("on").addClass("off");
+// 	});
+
+// $(".switch, .clapper").click(function () {
+// 	var room = $(this).closest(".room");
+// 	room.find(".lightbulb").trigger("light:toggle");
+// });
+
+// $("#master_switch").click(function () {
+// 	var lightbulbs = $(".lightbulb");
+
+// 	// Check if any lightbulbs are on
+// 	if (lightbulbs.is(".on")) {
+// 		lightbulbs.trigger("light:off");
+// 	} else {
+// 		lightbulbs.trigger("light:on");
+// 	}
+// });
+
