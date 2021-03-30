c5855b7cceb39ace6dadfb6a4b6105d1a518bd83
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-03-30 21:59:44 +0300

    curry

---
+++ functional-programming/curry.js
@@ -0,0 +1,42 @@
+const {curry} = require('ramda');
+let out;
+
+// const curry = (f) => (x) => (y) => f(x, y);
+
+// const add = (x, y) => x + y;
+
+// const toPair = (f) => ([x, y]) => f(x, y);
+// const fromPair = (f) => (x, y) => f([x, y]);
+
+// const flip = (f) => (y, x) => f(x, y);
+
+// const curry = f => x => y => f(x,y)
+
+// out1 = fromPair(toPair(add))(1, 2);
+// out2 = flip(add)(2, 2);
+// let curriedAdd = curry(add)
+
+// const increment = curriedAdd(1);
+// out3 = increment(2)
+
+// ***********************************
+
+// const uncurry = (f) => (x, y) => f(x)(y);
+
+const modulo = curry((x, y) => y % x);
+const isOdd = modulo(2);
+
+// out = idOdd(3);
+
+
+const filter1 = curry((f, xs) => xs.filter(f))
+
+const getOdds = filter1(isOdd)
+out = getOdds([1,2,3,4,5])
+
+const replace = curry((regex, replacement, str) => str.replace(regex, replacement))
+const replaceVowels = replace(/[AEUIO]/ig, '!');
+out2 = replaceVowels('Hey i haave words')
+
+console.log(out2);
+
